3ea8995c99840db00d89887fef62ae94891cc6c1
Eissanoor <eissaanoor@gmail.com>
2025-05-19 20:22:31 +0500

    Update Tailwind configuration and Navbar logo size
    - Changed the default sans font from "Inter" to "Axiforma" in tailwind.config.ts.
    - Adjusted the Navbar logo height from 28 to 24 for better visual alignment.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,7 +39,7 @@ const Navbar = () => {
           <div className="flex items-center">
             <Link to="/" className="text-2xl font-bold text-primary">
               {/* Glow<span className="text-accent">Tech</span> */}
-              <img src={logo} alt="" className="w-full h-28 object-cover cursor-pointer" />
+              <img src={logo} alt="" className="w-full h-24 object-cover cursor-pointer" />
             </Link>
           </div>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        sans: ["Inter", "sans-serif"],
+        sans: ["Axiforma", "sans-serif"],
         heading: ["Poppins", "sans-serif"],
       },
       colors: {
